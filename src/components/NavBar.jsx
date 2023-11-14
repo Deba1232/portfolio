a97d7b256b11440987fa1e3ae164546f92d07ae5
DeV <debanjan.das90@gmail.com>
2023-11-15 00:41:19 +0530

    add box shadow

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 shadow-xl">
       <div>
         <img src={Logo} alt="logo" style={{ width: "50px" }} />
       </div>
