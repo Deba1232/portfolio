85bcc3f54923498048e8f8c13b1b5fb48bdb745f
DeV <debanjan.das90@gmail.com>
2023-11-15 00:33:33 +0530

    change width for "xl" screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "xl" : "1100px",
+      }
+    },
   },
   plugins: [],
 };
