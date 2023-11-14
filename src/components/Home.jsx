e7612a81c52282ae81c8c17bd227c54b4372a61d
DeV <debanjan.das90@gmail.com>
2023-11-15 00:35:38 +0530

    add portfolio introduction

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[62.5rem] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-white">Hello, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Debanjan Das
+        </h1>
+        <h2 className="text-3xl sm:text-6xl font-bold text-[#8892b0]">
+          I'm a Front End Developer
+        </h2>
+        <div>
+          <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View projects <HiArrowNarrowRight className="ml-3" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
