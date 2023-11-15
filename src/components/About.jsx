77f88e130176ea4e3089e47673aa20914789b493
DeV <debanjan.das90@gmail.com>
2023-11-15 22:27:38 +0530

    change about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,7 @@ const About = () => {
         </div>
         <div className="max-w-[62.5rem] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>
-              Hi👋. I'm Debanjan, nice to meet you. Please take a look around.
-            </p>
+            <p>Hi👋, nice to meet you. Please take a look around.</p>
           </div>
           <div>
             <p>
