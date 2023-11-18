97fa71ce849468a02844ba6cb1e489c22a5da174
DeV <debanjan.das90@gmail.com>
2023-11-18 13:32:57 +0530

    remove h-screen style and change grid gap

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import sketchPadImg from "../assets/sketchPad.png";
 
 const Projects = () => {
   return (
-    <div className=" w-full md:h-screen text-gray-300 bg-[#0a192ff6]">
+    <div className=" w-full text-gray-300 bg-[#0a192ff6]">
       <div className="max-w-[62.5rem] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
@@ -14,7 +14,7 @@ const Projects = () => {
           <p className="py-6">Check out some of my projects</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 gap-5">
+        <div className="grid sm:grid-cols-2 gap-4">
           <div
             style={{ backgroundImage: `url(${boxOfficeAppImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
